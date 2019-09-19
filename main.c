b62ddb44137862b90f5eb1124b1ef38871e1b3f0
shinyeon57 <noeys57@sookmyung.ac.kr>
2019-09-19 15:32:10 +0900

    02

--- main.c
+++ main.c
@@ -4,6 +4,16 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("Hello Sookmyung EE!\n");
+	
+	int x;
+	short y;
+	float z;
+	double w;
+	
+	printf("byte of int : %d\n", sizeof(x));
+	printf("byte of short : %d\n",sizeof(y)) ;
+	printf("byte of float:%d\n",sizeof(z));
+	printf("byte of double:%d\n",sizeof(w));
+
 	return 0;
 }
